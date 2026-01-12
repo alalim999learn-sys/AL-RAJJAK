16d8221f7763fb4b39e1efa5bd6a71c8d4d3e766
AL-RAJJAK <alalim999learn@gmail.com>
2026-01-12 15:42:17 +0600

    update

--- component/HUUlayoutro.js
+++ component/HUUlayoutro.js
@@ -1,61 +1,71 @@
 // component/HUUlayoutro.js
-import ReactMarkdown from 'react-markdown';
-import remarkGfm from 'remark-gfm';
-import rehypeRaw from 'rehype-raw';
+import ReactMarkdown from 'react-markdown'
+import remarkGfm from 'remark-gfm'
+import rehypeRaw from 'rehype-raw'
 
 export default function HuuLayoutro({ frontmatter, content }) {
-  // Remove <header> and <h1> tags from raw HTML
   const sanitizedContent = content
     .replace(/<header.*?>.*?<\/header>/gi, '')
-    .replace(/<h1.*?>.*?<\/h1>/gi, '');
+    .replace(/<h1.*?>.*?<\/h1>/gi, '')
 
   return (
     <div style={{ padding: '0', background: '#fff', borderRadius: '12px' }}>
-      {/* Media query for small screens */}
-      <style>
-        {`
-          
+      <style>{`
+        .responsive-img {
+          max-width: 100%;
+          height: auto;
+          display: block;
+          margin: 1em 0;
+        }
 
-          @media (max-width: 600px) {
-            .responsive-img {
-              width: 100%;      
-            }
+        @media (max-width: 600px) {
+          .responsive-img {
+            width: 100%;
           }
-        `}
-      </style>
+        }
+      `}</style>
 
       <ReactMarkdown
         remarkPlugins={[remarkGfm]}
         rehypePlugins={[rehypeRaw]}
         components={{
-          h1: () => null, // remove h1 completely
-          h2: ({ node, ...props }) => (
+          h1: () => null,
+
+          h2: ({ children }) => (
             <h2
               style={{
                 textAlign: 'left',
                 margin: '1em 0',
-                fontWeight: 'bold'
+                fontWeight: 'bold',
               }}
-              {...props}
-            />
+            >
+              {children}
+            </h2>
           ),
-          p: ({ node, ...props }) => (
+
+          p: ({ children }) => (
             <p
               style={{
                 textAlign: 'left',
                 lineHeight: '1.6',
-                margin: '0.5em 0'
+                margin: '0.5em 0',
               }}
-              {...props}
-            />
+            >
+              {children}
+            </p>
           ),
-          img: ({ node, ...props }) => (
-            <img className="responsive-img" {...props} />
+
+          img: (props) => (
+            <img
+              {...props}
+              className="responsive-img"
+              alt={props.alt || ''}
+            />
           ),
         }}
       >
         {sanitizedContent}
       </ReactMarkdown>
     </div>
-  );
+  )
 }
